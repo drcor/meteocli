78abf0cf2c3eaa056cf18e0c8e4221af557065c5
Diogo Correia <53583855+drcor@users.noreply.github.com>
2023-09-08 14:19:43 +0100

    add description field with weather code info

--- src/main.rs
+++ src/main.rs
@@ -57,6 +57,7 @@ async fn main() {
     opts.hourly.push("relativehumidity_2m".into());
     opts.hourly.push("precipitation_probability".into());
     opts.hourly.push("precipitation".into());
+    opts.hourly.push("weathercode".into());
 
     // Get forecast
     let res = client.forecast(opts).await.unwrap();
@@ -64,7 +65,7 @@ async fn main() {
     if let Some(hour_forecast) = res.hourly {
         let dt = Local::now();
 
-        println!("{}", format!("{:<11}   {:<8}   {:<5}   {:<5}   {:<7}", "Date", "Temperat", "Humi%", "Prec%", "Precipitation"));
+        println!("{}", format!("{:<11}   {:<8}   {:<5}   {:<5}   {:<7}   {}", "Date", "Temperat", "Humi%", "Prec%", "Precipi", "Description"));
 
         for forecast in hour_forecast {
             let is_future_forecast = forecast.datetime.day() > dt.day() || forecast.datetime.hour() >= dt.hour();
@@ -76,12 +77,48 @@ async fn main() {
                 let relativehumidity_2m = forecast.values.get("relativehumidity_2m").unwrap();
                 let precipitation_probability = forecast.values.get("precipitation_probability").unwrap();
                 let precipitation = forecast.values.get("precipitation").unwrap();
+                let weathercode = forecast.values.get("weathercode").unwrap();
                 
                 print!("{} {}   ", format!("{:>5}", temperature_2m.value.to_string()), temperature_2m.unit.as_ref().unwrap());
                 print!("{} {}   ", format!("{:>3}", relativehumidity_2m.value.to_string()), relativehumidity_2m.unit.as_ref().unwrap());
                 print!("{} {}   ", format!("{:>3}", precipitation_probability.value.to_string()), precipitation_probability.unit.as_ref().unwrap());
-                println!("{} {}", format!("{:>4}", precipitation.value.to_string()), precipitation.unit.as_ref().unwrap());
+                print!("{} {}   ", format!("{:>4}", precipitation.value.to_string()), precipitation.unit.as_ref().unwrap());
+                println!(" {}", get_description(weathercode.value.as_u64().unwrap()));
             }
         }
     }
+}
+
+fn get_description(weathercode: u64) -> &'static str {
+    return match weathercode {
+        0 => "Clear sky",
+        1 => "Mainly clear",
+        2 => "Partly cloudy",
+        3 => "Overcast",
+        45 => "Fog",
+        48 => "Deposition rime fog",
+        51 => "Drizzle: Light intensity",
+        53 => "Drizzle: Moderate intensity",
+        55 => "Drizzle: Dense intensity",
+        56 => "Freezing Drizzle: Light intensity",
+        57 => "Freezing Drizzle: Dense intensity",
+        61 => "Rain: Slight intensity",
+        63 => "Rain: Moderate intensity",
+        65 => "Rain: Heavy intensity",
+        66 => "Freezing Rain: Light intensity",
+        67 => "Freezing Rain: Heavy intensity",
+        71 => "Snow fall: Slight intensity",
+        73 => "Snow fall: Moderate intensity",
+        75 => "Snow fall: Heavy intensity",
+        77 => "Snow grains",
+        80 => "Rain showers: Slight",
+        81 => "Rain showers: Moderate",
+        82 => "Rain showers: Violent",
+        85 => "Snow showers slight",
+        86 => "Snow showers heavy",
+        95 => "Thunderstorms: Slight or heavy",
+        96 => "Thunderstorms with slight hail",
+        99 => "Thunderstorms with heavy hail",
+        _ => "Uknown weather code...",
+    };
 }
